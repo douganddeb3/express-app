085f7e6a7587659ca42f9e9f20b4bb9d606078ba
Doug Nelson <douganddeb3@msn.com>
2017-11-15 13:13:17 -0500

    My first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+
+var redis = require('redis');
+var client = redis.createClient();
+
+client.hset('songs', 'Doris Day', 'Sentimental Journey');
+client.hset('songs', 'Monkees', 'DayDream Believer');
+client.hset('songs', ' Carl Gustav Boberg', 'How Great Thou Art');
+
+app.get('/', function(request, response){
+	response.send('OK');
+});
+
+
+//var songs = {'Doris Day':'Sentimental Journey', 'Monkees': 'Daydream Believer', ' Carl Gustav Boberg': 'How Great Thou Art'};
+app.get('/songs', function(request, response){
+		client.hkeys('songs', function(error, names){
+		if(error) throw error;
+		response.json(names);	
+		});
+		//response.json(Object.keys(songs));
+});
+
+app.get('/songs1/:name', function(request, response){
+	var name = request.params.name;
+	client.hget('songs', name, function(error, reply){
+	console.log(reply);
+	response.json(reply);	
+	}); 
+		//response.json(songs[request.params.name]);
+});
+
+
+
+app.post('/songs', parseUrlencoded, function(request, response){
+	var data = request.body;
+	if(!data.name || !data.song){
+		response.sendStatus(400);
+		return false;
+	}
+	client.hset('songs', data.name, data.song);
+	var song3 = data.name;
+	response.json(song3);	
+
+	//songs[data.name] = data.song;
+	//response.status(200).json(data.name);	
+});
+
+app.get('/songs/:name', function(request, response){
+	var name = request.params.name;
+	client.hget('songs', 'name', function(error, obj){
+	response.json(obj);	
+	});
+	
+});
+
+app.delete('/songs/:name', function(request, response){
+	var name = request.params.name;
+	client.hdel('songs', name, function(error) {
+      if(error) throw error;
+      response.json(name);
+    });
+});
+
+app.listen(3000);
---
+++ public/client.js
@@ -0,0 +1,44 @@
+$(function(){
+$.get('/songs', appendToList);
+
+function appendToList(songs) {
+var list = [];
+for(var i in songs){
+list.push($('<li><a href="#" data-song='+songs[i]+'>x&nbsp;</a><a href="/songs1/'+songs[i]+'" id="link">' +songs[i]+ '</a></li>' ));  //{ text: cities[i] }
+}
+$('.song-list').append(list);
+}
+
+$('.song-list').on('click', 'a[data-song]', function (event) {
+    if(!confirm('Are you sure ?')){
+      return false;
+    }
+
+    var target = $(event.currentTarget);
+
+    $.ajax({
+      type: 'DELETE',
+      url: '/songs/' + target.data('song')
+    }).success(function () {
+      target.parents('li').remove();
+    });
+  });
+
+
+$('form').on('submit', function(event){
+	event.preventDefault();
+	var form = $(this);
+	var songData = form.serialize();
+	$.ajax({
+		type: 'POST', url: '/songs', data: songData
+	}).error(function(){
+    	alert("fill in the form");
+	}).success(function(song4){	
+		$('.song-list').append('<li><a href="#" data-song='+song4+'>x&nbsp;</a><a href="/songs1/'+song4+'">' +song4+ '<a></li>');
+		form.trigger('reset');
+	});
+	
+	
+}); 
+
+});  
