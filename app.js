b8453768c78e6fdae69419590d7bf6e84b50841b
Doug Nelson <douganddeb3@msn.com>
2017-11-22 18:45:32 -0500

    correct order of routes

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ app.use(express.static('public'));
 
 var songs = require('./routes/songs');
 app.use('/songs', songs);
+
+var songs1 = require('.routes/songs1');
+app.use('/songs1', songs1);
+
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
 	console.log('Listening on port '+ port);
 });
 
-var songs1 = require('.routes/songs1');
-app.use('/songs1', songs1);
+
 
 
 //var songs = {'Doris Day':'Sentimental Journey', 'Monkees': 'Daydream Believer', ' Carl Gustav Boberg': 'How Great Thou Art'};
