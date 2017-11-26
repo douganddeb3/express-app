819ecef4b83c9d61d254461b63e9c984f1fae967
Doug Nelson <douganddeb3@msn.com>
2017-11-26 12:28:57 -0500

    list by song

--- app.js
+++ app.js
@@ -17,20 +17,6 @@ app.listen(port, function(){
 
 
 
-//var songs = {'Doris Day':'Sentimental Journey', 'Monkees': 'Daydream Believer', ' Carl Gustav Boberg': 'How Great Thou Art'};
-
-
-/*app.get('/songs1/:name', function(request, response){
-	var name = request.params.name;
-	client.hget('songs', name, function(error, reply){
-		response.render('show.ejs',
-			{ song: {name: name, songInd: reply}});
-	//console.log(reply);
-	//response.json(reply);	
-	}); 
-		//response.json(songs[request.params.name]);
-});
-*/
 
 
 
